85cc54485f30b95418c1ea36e4c17a9195abb312
buer <19681022+weiyouwozuiku@users.noreply.github.com>
2021-03-30 16:54:52 +0800

    acwing#77

--- Cpp/acwing/[77]翻转单词顺序.cpp
+++ Cpp/acwing/[77]翻转单词顺序.cpp
@@ -2,3 +2,16 @@
 // Created by king on 2021/3/30.
 //
 
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        for(int i=0;i<s.size();i++){
+            int j=i+1;
+            while(s[j]!=' '&&j<s.size()) j++;
+            reverse(s.begin()+i,s.begin()+j);
+            i=j;
+        }
+        return s;
+    }
+};
