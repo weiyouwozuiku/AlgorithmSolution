af2f35516d54b497040423f004997a7545a09271
buer <19681022+weiyouwozuiku@users.noreply.github.com>
2021-04-12 11:19:01 +0800

    acwing#790

---
+++ Cpp/acwing/[790]数的三次方根.cpp
@@ -0,0 +1,25 @@
+//
+// Created by king on 2021/4/12.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x;
+    scanf("%lf", &x);
+    if (x < 0) {
+        printf("-");
+        x = -x;
+    }
+    double l = 0, r = x, mid;
+    if (x<1) r=1;
+    while (r - l >= 1e-8) {
+        mid = (r + l) / 2;
+        if (mid * mid * mid > x) r = mid;
+        else l = mid;
+    }
+    printf("%lf", mid);
+    return 0;
+}
