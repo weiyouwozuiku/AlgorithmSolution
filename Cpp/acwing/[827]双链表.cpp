1057b4d7c2cabcee5ce28aedd6142cf8a7831669
buer <19681022+weiyouwozuiku@users.noreply.github.com>
2021-05-05 11:43:33 +0800

    acwing#827

---
+++ Cpp/acwing/[827]双链表.cpp
@@ -0,0 +1,69 @@
+//
+// Created by king on 2021/5/2.
+//
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int N = 1e6 + 10;
+int l[N], r[N], e[N];
+int idx;
+
+void init() {
+    //0代表起始端点，1代表结束端点
+    //真实数字从idx==2开始存放
+    idx = 2;
+    l[1] = 0;
+    r[0] = 1;
+}
+
+//此函数的逻辑是插入k的右侧节点
+//若需要实现插入k的左侧节点只需传入l[k]即可
+void add(int k, int v) {
+    e[idx] = v;
+    l[idx] = k;
+    r[idx] = r[k];
+    l[r[k]] = idx;
+    r[k] = idx;
+    ++idx;
+}
+
+void remove(int k) {
+    r[l[k]] = r[k];
+    l[r[k]] = l[k];
+}
+
+
+int main() {
+    int n;
+    init();
+    scanf("%d", &n);
+    while (n--) {
+        char op[3];
+        int x, y;
+        scanf("%s", &op);
+        if (!strcmp("L", op)) {
+            scanf("%d", &x);
+            add(0, x);
+        }
+        if (!strcmp("R", op)) {
+            scanf("%d", &x);
+            add(l[1], x);
+        }
+        if (!strcmp("D", op)) {
+            scanf("%d", &x);
+            remove(x + 1);
+        }
+        if (!strcmp("IL", op)) {
+            scanf("%d%d", &x, &y);
+            add(l[x + 1], y);
+        }
+        if (!strcmp("IR", op)) {
+            scanf("%d%d", &x, &y);
+            add(x + 1, y);
+        }
+    }
+    for (int i = r[0]; i != 1; i = r[i]) printf("%d ", e[i]);
+    return 0;
+}
