5f6c603726ac48bb11a0272cf44fb0704bf31eb3
buer <19681022+weiyouwozuiku@users.noreply.github.com>
2021-08-17 23:02:10 +0800

    leetcode#21

---
+++ Cpp/leetcode/editor/cn/[21]合并两个有序链表.cpp
@@ -0,0 +1,71 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+// Related Topics 递归 链表 👍 1852 👎 0
+
+/**
+ * 本题的解题思路：
+ * 本题就是简单的二路归并问题
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        //结果链表的头节点设置为虚拟节点避免
+        auto dummy = new ListNode(-1), tail = dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail = tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail = tail->next = l2;
+                l2 = l2->next;
+            }
+        }
+        if (l1) tail->next = l1;
+        if (l2) tail->next = l2;
+        return dummy->next;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
