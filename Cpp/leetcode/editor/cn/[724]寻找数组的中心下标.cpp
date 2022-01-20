20339c5edba6f4f3522e809ecdfb2432da10b6be
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2022-01-21 02:33:20 +0800

    leetcode#1480&&leetcode#1991

--- Cpp/leetcode/editor/cn/[724]寻找数组的中心下标.cpp
+++ Cpp/leetcode/editor/cn/[724]寻找数组的中心下标.cpp
@@ -57,10 +57,10 @@
 class Solution {
 public:
     int pivotIndex(vector<int> &nums) {
-        int total=accumulate(nums.begin(),nums.end(),0);
-        for(int i=0,s=0;i<nums.size();i++){
-            if(s==total-nums[i]-s) return i;
-            else s+=nums[i];
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        for (int i = 0, s = 0; i < nums.size(); i++) {
+            if (s == total - nums[i] - s) return i;
+            else s += nums[i];
         }
         return -1;
     }
