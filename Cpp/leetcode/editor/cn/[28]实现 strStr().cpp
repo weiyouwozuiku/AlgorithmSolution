674268559400f6ce9ee73278e7cb06d098c9bca8
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2022-02-08 23:21:40 +0800

    增加leetcode#28注释

--- Cpp/leetcode/editor/cn/[28]实现 strStr().cpp
+++ Cpp/leetcode/editor/cn/[28]实现 strStr().cpp
@@ -55,6 +55,7 @@ public:
         // 这里最后多出来的元素用来标记匹配成功的情况
         ne.resize(n + 1);
         for (int i = 0, j = ne[0] = -1; i < n; ne[++i] = ++j) {
+            // ~j表示j为-1的情况
             while (~j && needle[i] != needle[j]) j = ne[j];
         }
     }
@@ -69,6 +70,8 @@ public:
             if (j == n) {
                 res = i - n + 1;
                 break;
+                // 如果是查找多个
+                // 只需要j=ne[j];即可，ne[n]==0，即从匹配串的第0位开始重新匹配
             }
         }
         return res;
