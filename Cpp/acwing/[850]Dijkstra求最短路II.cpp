864b95d0a4f32434450d428aec6f261fc7db8194
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2022-04-14 01:59:06 +0800

    更新堆优化的dijkstra算法

--- Cpp/acwing/[850]Dijkstra求最短路II.cpp
+++ Cpp/acwing/[850]Dijkstra求最短路II.cpp
@@ -1,18 +1,39 @@
 //
 // Created by king on 2022/4/12.
 //
-#include <iostream>
 #include <cstring>
+#include <queue>
 
 using namespace std;
 int n, m;
 const int N = 1e5 + 10;
 int h[N], e[N], ne[N], w[N], dist[N], idx;
 bool st[N];
+typedef pair<int, int> PII;
 
-int dijkstra(int index){
-
-
+int dijkstra(int index) {
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+    // 创建小根堆
+    priority_queue<PII, vector<PII>, greater<PII>> heap;
+    heap.push({0, 1});
+    while (heap.size()) {
+        auto t = heap.top();
+        heap.pop();
+        int ver = t.second, distance = t.first;
+        // 跳过冗余节点
+        if (st[ver]) continue;
+        st[ver] = true;
+        for (int i = h[ver]; i != -1; i = ne[i]) {
+            int j = e[i];
+            if (dist[j] > distance + w[i]) {
+                dist[j] = distance + w[i];
+                heap.push({dist[j], j});
+            }
+        }
+    }
+    if (dist[n] == 0x3f3f3f3f) return -1;
+    else return dist[n];
 }
 
 void add(int a, int b, int c) {
