6e894d0f232db7a2285075640dcf7e505259c0bf
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2022-11-15 09:55:16 +0800

    leetcode#86

---
+++ Cpp/leetcode/editor/cn/[86]分隔链表.cpp
@@ -0,0 +1,62 @@
+//给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。 
+//
+// 你应当 保留 两个分区中每个节点的初始相对位置。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,4,3,2,5,2], x = 3
+//输出：[1,2,2,4,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [2,1], x = 2
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 200] 内 
+// -100 <= Node.val <= 100 
+// -200 <= x <= 200 
+// 
+//
+// Related Topics 链表 双指针 👍 648 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* dummy1=new ListNode(-1);
+        ListNode* p1=dummy1;
+        ListNode* dummy2=new ListNode(-1);
+        ListNode* p2=dummy2;
+        while(head){
+            if(head->val<x) p1=p1->next=head;
+            else p2=p2->next=head;
+            head=head->next;
+        }
+        p2->next= nullptr;
+        p1->next=dummy2->next;
+        return dummy1->next;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
