5b2ef7cb3cd95bfd447b45d580b706fcf053a54e
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2023-01-18 01:09:52 +0800

    acwing#866 && 867

--- Cpp/acwing/[866]试除法判断质数.cpp
+++ Cpp/acwing/[866]试除法判断质数.cpp
@@ -1,3 +1,26 @@
 //
 // Created by king on 2023/1/7.
 //
+#include <cstdio>
+
+bool is_prime(int x) {
+    if (x < 2)return false;
+    for (int i = 2; i <= x / i; ++i) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n,x;
+    scanf("%d", &n);
+    for(int i=0;i<n;++i){
+        scanf("%d",&x);
+        if (is_prime(x)) {
+            puts("Yes");
+        } else {
+            puts("No");
+        }
+    }
+    return 0;
+}
--- Cpp/acwing/[867]分解质因数.cpp
+++ Cpp/acwing/[867]分解质因数.cpp
@@ -1,3 +1,29 @@
 //
 // Created by king on 2023/1/7.
 //
+#include <cstdio>
+
+void divide(int x) {
+    for (int i = 2; i <= x / i; ++i) {
+        if (x % i == 0) {
+            int b = 0;
+            while (x % i == 0) {
+                x /= i;
+                b += 1;
+            }
+            printf("%d %d\n", i, b);
+        }
+    }
+    if (x > 1) printf("%d 1\n", x);
+    puts("");
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &x);
+        divide(x);
+    }
+    return 0;
+}
