41549c3d74c74794641f8f6095ab66c36f2247a9
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2023-03-08 09:58:12 +0800

    save

---
+++ Cpp/leetcode/editor/cn/[199]二叉树的右视图.cpp
@@ -0,0 +1,73 @@
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: [1,2,3,null,5,null,4]
+//输出: [1,3,4]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,null,3]
+//输出: [1,3]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: []
+//输出: []
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [0,100] 
+// 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 826 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode *root) {
+        vector<int> res;
+        if (!root) return {};
+        queue < TreeNode * > q;
+        q.push(root);
+        while (q.size()) {
+            int len = q.size();
+            for (int i = 0; i < len; ++i) {
+                auto p = q.front();
+                q.pop();
+                if (p->left) q.push(p->left);
+                if (p->right) q.push(p->right);
+                if (i == len - 1) res.push_back(p->val);
+            }
+        }
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
