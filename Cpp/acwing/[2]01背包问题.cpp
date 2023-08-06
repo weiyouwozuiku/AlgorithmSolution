e98a667c1a73963e272ab445542f08ef963d6f17
weiyouwozuiku <19681022+weiyouwozuiku@users.noreply.github.com>
2023-08-07 00:34:34 +0800

    save

---
+++ Cpp/acwing/[2]01背包问题.cpp
@@ -0,0 +1,39 @@
+//
+// Created by king on 2023/7/6.
+//
+#include <iostream>
+
+using namespace std;
+const int N = 1001;
+int v[N], w[N];
+int f[N][N];
+int ff[N];
+
+void twoDimension(int n, int m) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            f[i][j] = f[i - 1][j];
+            if (j >= v[i]) {
+                f[i][j] = max(f[i][j], f[i - 1][j - v[i]] + w[i]);
+            }
+        }
+    }
+    printf("%d\n", f[n][m]);
+}
+
+void oneDimension(int n, int m) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = m; j >= v[i]; --j) {
+            ff[j] = max(ff[j], ff[j - v[i]] + w[i]);
+        }
+    }
+    printf("%d\n", ff[m]);
+}
+
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; ++i) scanf("%d%d", &v[i], &w[i]);
+    oneDimension(n, m);
+    return 0;
+}
